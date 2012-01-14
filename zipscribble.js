f4485f9acca67f840e1f091bc91a1f5604f77209
Robert Kosara <rkosara@me.com>
2012-01-14 00:01:34 -0800

    Add BSD license to relevant files.

--- zipscribble.js
+++ zipscribble.js
@@ -1,3 +1,18 @@
+/*
+	Copyright (c) 2012, Robert Kosara <rkosara@me.com>
+	
+	Permission to use, copy, modify, and/or distribute this software for any
+	purpose with or without fee is hereby granted, provided that the above
+	copyright notice and this permission notice appear in all copies.
+	
+	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+	WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+	MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+	ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+	WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+	ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+	OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
 
 var map;
 
@@ -26,7 +41,7 @@ function initMap(){
 	
 	mapLayer = po.image()
 	    .url(po.url("http://{S}tile.cloudmade.com"
-	    + "/b12a0bb2f7844dda8b214fda256a6bbd"
+	    + "/b12a0bb2f7844dda8b214fda256a6bbd" // please get your own API key if you use this somewhere else!
 	    + "/998/256/{Z}/{X}/{Y}.png")
 	    .hosts(["a.", "b.", "c.", ""]));
 	
