2dc9806607f9b2eaf18eedbeea82c44bd76d5163
Robert Kosara <rkosara@me.com>
2021-12-08 23:13:16 -0800

    Highlighting by state

--- scrbblr/src/Navigator.svelte
+++ scrbblr/src/Navigator.svelte
@@ -12,9 +12,10 @@
 	export let numZIPs;
 	export let range = [];
 
-	let xScale = scaleLinear([0, numZIPs], [0, width]);
+	let xScale = scaleLinear([0, numZIPs], [1, width]);
 
-	let prevDigit = -1;
+	let activeDigit = -1;
+	let activeState = -1;
 
 	let svg;
 	let point;
@@ -29,24 +30,42 @@
 		point.y = e.clientY;
 		point = point.matrixTransform(svg.getScreenCTM().inverse());
 
-		// console.log(point.x);
-		let digit = 9;
-		while (point.x < xScale(digits[digit])) {
-			digit -= 1;
-		}
-		if (digit != prevDigit) {
-			if (digit < 9)
-				range = [digits[digit], digits[digit+1]];
-			else
-				range = [digits[digit], numZIPs];
+		// console.log(point.x, point.y-y);
+		if (point.y-y <= 20) {
+			let digit = 9;
+			while (point.x < xScale(digits[digit])) {
+				digit -= 1;
+			}
+			if (digit != activeDigit) {
+				if (digit < 9)
+					range = [digits[digit], digits[digit+1]];
+				else
+					range = [digits[digit], numZIPs];
 
-			prevDigit = digit;
+				activeDigit = digit;
+			}
+		} else if (point.y-y >= 30) {
+			let mouseOffset = xScale.invert(point.x);
+			let state = states.length-1;
+			while (states[state].offset > mouseOffset) {
+				state -= 1;
+			}
+			if (state !== activeState) {
+				if (state < states.length-1)
+					range = [states[state].offset, states[state+1].offset];
+				else
+					range = [states[state].offset, numZIPs];
+				activeState = state;
+			}
+		} else {
+			activeDigit = -1;
+			activeState = -1;
 		}
 	}
 
 	function mouseLeave() {
 		range = [];
-		prevDigit = -1;
+		activeDigit = -1;
 	}
 
 </script>
@@ -54,14 +73,22 @@
 <g transform={`translate(${x}, ${y})`}>
 	<rect x={0} y={0} {width} {height} class="background"
 		on:mousemove={mouseMove} on:mouseleave={mouseLeave} />
-	<rect x={0} y={height-40} width={width} height={20} class="bar" />
+	<rect x={0} y={height-30} width={width} height={10} class="bar" />
+	{#if activeDigit >= 0}
+		<rect x={xScale(digits[activeDigit])} y={0} width={activeDigit < 9 ? xScale(digits[activeDigit+1])-xScale(digits[activeDigit]) : width-xScale(digits[activeDigit])} height={20} class="active" />
+	{/if}
 	{#each digits as d, i}
-		<line x1={xScale(d)} y1={0} x2={xScale(d)} y2={height-40} />
-		<text x={i < 9 ? (xScale(d)+xScale(digits[i+1]))/2 : (xScale(d)+width)/2 } y={height-42} >{i}</text>
+		<line x1={xScale(d)} y1={0} x2={xScale(d)} y2={height-30} />
+		<text x={i < 9 ? (xScale(d)+xScale(digits[i+1]))/2 : (xScale(d)+width)/2 } y={height-37} >{i}</text>
 	{/each}
+	{#if activeState >= 0}
+		<rect x={xScale(states[activeState].offset)} y={30} width={activeState < states.length-1 ? xScale(states[activeState+1].offset)-xScale(states[activeState].offset) : width-xScale(states[activeState].offset)} height={20} class="active" />
+	{/if}
 	{#each states as state, i}
 		<line x1={xScale(state.offset)} y1={height-20} x2={xScale(state.offset)} y2={height} />
-		<text x={i < states.length-1 ? (xScale(state.offset)+xScale(states[i+1].offset))/2 : (xScale(state.offset)+width)/2 } y={height-5} >{state.state}</text>
+		{#if i < states.length-1 && xScale(states[i+1].offset)-xScale(state.offset) > 20}
+			<text x={i < states.length-1 ? (xScale(state.offset)+xScale(states[i+1].offset))/2 : (xScale(state.offset)+width)/2 } y={height-5} >{state.state}</text>
+		{/if}
 	{/each}
 </g>
 
@@ -81,6 +108,11 @@
 		fill: darkgray;
 		font-size: 8pt;
 		text-anchor: middle;
+		pointer-events: none;
+	}
+
+	rect.active {
+		fill: lightsteelblue;
 		pointer-events: none;
 	}
 
