d0125e15f83b5e844319dd550aaa7abf32ecb970
Robert Kosara <rkosara@me.com>
2021-12-11 22:57:50 -0800

    Visual cleanup, state highlighting

--- scrbblr/src/Navigator.svelte
+++ scrbblr/src/Navigator.svelte
@@ -11,7 +11,7 @@
 	export let zoomRange = [];
 	export let highlightRange = [];
 
-	let xScale = scaleLinear([0, numZIPs], [1, width]);
+	let xScale = scaleLinear([0, numZIPs], [0, width]);
 
 	let activeFirst = -1;
 	let activeSecond = -1;
@@ -38,6 +38,8 @@
 			}
 			if (second >= 0) {
 				highlightRange = [digits[first].secondDigits[second].startOffset, digits[first].secondDigits[second].endOffset];
+			} else if (state >= 0) {
+				highlightRange = [digits[first].states[state].startOffset, digits[first].states[state].endOffset];
 			} else {
 				highlightRange = zoomRange;
 			}
@@ -50,6 +52,7 @@
 		}
 		activeFirst = first;
 		activeSecond = second;
+		activeState = state;
 	}
 
 
@@ -58,18 +61,22 @@
 <g transform={`translate(${x}, ${y})`}>
 	{#each digits as d, i}
 		<rect x={xScale(d.startOffset)} y={0} width={xScale(d.endOffset)-xScale(d.startOffset)} height={20}
-			class={activeFirst === i ? 'active' : 'bar'}
+			class:active={activeFirst === i}
 			on:mouseenter={() => setActiveDigit(i)} on:mouseleave={() => setActiveDigit(-1)} />
-		<line x1={xScale(d.startOffset)} y1={0} x2={xScale(d.startOffset)} y2={20} />
-		<text x={(xScale(d.startOffset)+xScale(d.endOffset))/2} y={13} >{i}</text>
+		{#if i > 0}
+			<line x1={xScale(d.startOffset)} y1={0} x2={xScale(d.startOffset)} y2={20} />
+		{/if}
+		<text x={(xScale(d.startOffset)+xScale(d.endOffset))/2} y={13} class:active={activeFirst === i} >{i}</text>
 		{#each d.secondDigits as second, s}
 			<rect x={xScale(second.startOffset)} y={20} width={xScale(second.endOffset)-xScale(second.startOffset)} height={20}
-				class={activeFirst === i && activeSecond === s ? 'active' : 'bar'}
+				class:active={activeFirst === i && activeSecond === s}
 				on:mouseenter={() => setActiveDigit(i, s)} on:mouseleave={() => setActiveDigit(i, -1)} />
 			<line x1={xScale(second.startOffset)} y1={20} x2={xScale(second.startOffset)} y2={40} />
 		{/each}
 		{#each d.states as state, s}
-			<rect x={xScale(state.startOffset)} y={40} width={xScale(state.endOffset)-xScale(state.startOffset)} height={20} class={activeState === state ? 'active' : 'bar'} />
+			<rect x={xScale(state.startOffset)} y={40} width={xScale(state.endOffset)-xScale(state.startOffset)} height={20}
+				class:active={(activeFirst === i && activeState === s) || (activeState >= 0 && activeState < digits[activeFirst].states.length && state.state === digits[activeFirst].states[activeState].state)}
+				on:mouseenter={() => setActiveDigit(i, -1, s)} on:mouseleave={() => setActiveDigit(i, -1, -1)} />
 			<line x1={xScale(state.startOffset)} y1={40} x2={xScale(state.startOffset)} y2={60} />
 			{#if xScale(state.endOffset)-xScale(state.startOffset) > 20}
 				<text x={(xScale(state.startOffset)+xScale(state.endOffset))/2} y={55} >{state.state}</text>
@@ -77,18 +84,17 @@
 		{/each}
 		<line x1={0} y1={20} x2={width} y2={20} />
 		<line x1={0} y1={40} x2={width} y2={40} />
-		<line x1={width-0.5} y1={0} x2={width-0.5} y2={60} />
 	{/each}
 </g>
 
 <style>
-	rect.bar {
-		fill: white;
+	rect {
+		fill: lightgray;
 		stroke: none;
 	}
 
 	line {
-		stroke: lightgray;
+		stroke: white;
 		pointer-events: none;
 	}
 
@@ -99,17 +105,22 @@
 		pointer-events: none;
 	}
 
+	text.active {
+		fill: black;
+		font-weight: bold;
+	}
+
 	rect.active {
 		fill: lightsteelblue;
 	}
 
 	@media (prefers-color-scheme: dark) {
-		rect.bar {
-			fill: #222;
+		rect {
+			fill: #333;
 		}
 
 		line {
-			stroke: #444;
+			stroke: #222;
 		}
 	}
 </style>
