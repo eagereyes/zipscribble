ae2dca722bd0b18ffa69f1d617b4209df7fb7c8d
Robert Kosara <rkosara@me.com>
2026-04-07 11:22:35 -0700

    Remove unneeded adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: null
-		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH ?? '')
 		}
